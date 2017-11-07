064b5dff9e3c5bac8239738b6b28b2cd72acc9d7
jojosif <josemorgado@Joses-MacBook-Pro.local>
2017-11-07 19:43:23 +0000

    word length

---
+++ assignment2/word_length_reducer/src/WordLengthMapper.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordLengthMapper extends
+		Mapper<Object, Text, IntWritable, IntWritable> {
+
+	private final IntWritable one = new IntWritable(1);
+	private final IntWritable length = new IntWritable(1);
+
+	public void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+			
+		if(value.toString().split(";").length == 4) {
+		
+		String[] line = value.toString().split(";");
+			if(length.set(line[2].length())){
+			context.write(length, one);
+
+	}
+}
+}
+}
