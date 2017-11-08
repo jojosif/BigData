437043f3104cd2ad73738f6f1008e19a889da2ab
jojosif <josemorgado@Joses-MacBook-Pro.local>
2017-11-08 18:20:58 +0000

    word_length_mapper

--- assignment2/word_length_mapper/src/WordLengthMapper.java
+++ assignment2/word_length_mapper/src/WordLengthMapper.java
@@ -5,11 +5,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class WordLengthMapper extends
-		Mapper<Object, Text, DoubleWritable, IntWritable> {
+		Mapper<Object, Text, IntWritable, IntWritable> {
 
 	private final IntWritable one = new IntWritable(1);
 	private final IntWritable length = new IntWritable(1);
-    private final DoubleWritable tir = new DoubleWritable(1);
+    private final IntWritable tir = new IntWritable(1);
 
 	public void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
@@ -20,8 +20,7 @@ public class WordLengthMapper extends
 			length.set(line[2].length());	 
 			double number = line[2].length() ;
 				if (number < 140){
-				    double size = (number % 5);
-				    tir.set(size);
+				    tir.set = (number % 5);
 					context.write(tir, one);
 					
 
@@ -29,3 +28,4 @@ public class WordLengthMapper extends
 }
 }
 }
+
