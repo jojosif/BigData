fd6bfd9656054c04277d37de207551af3d122814
jojosif <josemorgado@Joses-MacBook-Pro.local>
2017-11-08 18:12:31 +0000

    word_length_mapper

--- assignment2/word_length_mapper/src/WordLengthMapper.java
+++ assignment2/word_length_mapper/src/WordLengthMapper.java
@@ -9,7 +9,7 @@ public class WordLengthMapper extends
 
 	private final IntWritable one = new IntWritable(1);
 	private final IntWritable length = new IntWritable(1);
-    private final IntWritable tir = new IntWritable(1)
+    private final IntWritable tir = new IntWritable(1);
 
 	public void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
