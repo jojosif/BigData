05ae30fe5d93849b6cdab5718499e97ba65ec517
jojosif <josemorgado@Joses-MacBook-Pro.local>
2017-11-08 18:58:52 +0000

    word_length_mapper

--- assignment2/word_length_mapper/src/WordLengthMapper.java
+++ assignment2/word_length_mapper/src/WordLengthMapper.java
@@ -5,27 +5,26 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class WordLengthMapper extends
-		Mapper<Object, Text, IntWritable, IntWritable> {
+        Mapper<Object, Text, IntWritable, IntWritable> {
 
-	private final IntWritable one = new IntWritable(1);
-	private final IntWritable length = new IntWritable(1);
+    private final IntWritable one = new IntWritable(1);
+    private final IntWritable length = new IntWritable(1);
     private final IntWritable tir = new IntWritable(1);
 
-	public void map(Object key, Text value, Context context)
-			throws IOException, InterruptedException {
-			
-		if(value.toString().split(";").length == 4) {
-		
-			String[] line = value.toString().split(";");	
-			length.set(line[2].length());	 
-			double number = line[2].length() ;
-				if (number < 140){
-				    tir.set = (number % 5);
-					context.write(tir, one);
-					
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
 
-	}
-}
-}
-}
+        if(value.toString().split(";").length == 4) {
 
+            String[] line = value.toString().split(";");
+            length.set(line[2].length());
+            double number = line[2].length() ;
+            tir.set((int) (number / 5));
+            if (number < 141){
+                context.write(tir, one);
+
+
+            }
+        }
+    }
+}
